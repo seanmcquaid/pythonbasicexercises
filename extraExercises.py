f20b4c42506c2f39dd42044e245657abe140ddcf
seanmcquaid <sean.mcquaid2@verizon.net>
2018-11-17 13:18:25 -0500

    Finished while loop exercise

--- extraExercises.py
+++ extraExercises.py
@@ -182,7 +182,18 @@ print "Amount per person: " + str(split)
 
 # $ python 1_to_10.py 1 2 3 4 5 6 7 8 9 10
 
-# 10. How many coins?
-# Write a program that will prompt you for how many coins you want. Initially you have no coins. It will ask you if you want a coin? If you type "yes", it will give you one coin, and print out the current tally. If you type no, it will stop the program. Example run:
+i = 0
+while i <= 10:
+    print i 
 
-# $ python coins.py You have 0 coins. Do you want another? yes You have 1 coins. Do you want another? yes You have 2 coins. Do you want another? no Bye
+
+# 10. How many coins?
+# Write a program that will prompt you for how many coins you want. 
+# Initially you have no coins. It will ask you if you want a coin? 
+# If you type "yes", it will give you one coin, and print out the current tally. 
+# If you type no, it will stop the program. Example run:
+
+# $ python coins.py You have 0 coins. 
+# Do you want another? yes You have 1 coins. 
+# Do you want another? yes You have 2 coins. 
+# Do you want another? no Bye
